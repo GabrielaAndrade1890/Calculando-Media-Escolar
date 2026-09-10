9f8cdfc8521ed2c12f548739625fc18116d2dff5
Gabriela Andrade <gabrielaandrade1890@gmail.com>
2026-09-10 16:25:35 -0300

    feature: codigo-completo
    codigo completo e corrigido erros + adicionando nota de desempenho pela sua media escolar.

--- calculando-media-escolar.py
+++ calculando-media-escolar.py
@@ -1,3 +1,4 @@
+print()
 print('CALCULANDO MÉDIA ESCOLAR')
 print('==========================')
 
@@ -10,6 +11,31 @@ nota4 = float(input('Digite sua quarta nota: '))
 
 media = ((nota1 + nota2 + nota3 + nota4)/4)
 
-print(f'{nome_aluno.title()}, sua média foi {media}')
+print()
 
-# testando breach nova, teste teste
+if media == 10.0:
+    print(f'{nome_aluno.title()}, sua média foi {media}')
+    print('Seu desempenho escolar foi: Excelente💙')
+elif media >= 9.5:
+    print(f'{nome_aluno.title()}, sua média foi {media}')
+    print('Seu desempenho escolar foi: Excelente💙')
+elif media >= 8.5:
+    print(f'{nome_aluno.title()}, sua média foi {media}')
+    print('Seu desempenho escolar foi: Bom💚')
+elif media >= 7.5:
+    print(f'{nome_aluno.title()}, sua média foi {media}')
+    print('Seu desempenho escolar foi: Bom💚')
+elif media >= 6.5:
+    print(f'{nome_aluno.title()}, sua média foi {media}')
+    print('Seu desempenho escolar foi: Bom💚')
+elif media >= 5.5:
+    print(f'{nome_aluno.title()}, sua média foi {media}')
+    print('Seu desempenho escolar foi: Critico💛')
+elif media >= 4.5:
+    print(f'{nome_aluno.title()}, sua média foi {media}')
+    print('Seu desempenho escolar foi: Critico💛')
+else:
+    print(f'{nome_aluno.title()}, sua média foi {media}')
+    print('Seu desempenho escolar foi: Ruim💔')
+
+print()
