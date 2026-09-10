2f12399b6dcbe7e41a932ff5bd0df2fcd71d8126
Gabriela Andrade <gabrielaandrade1890@gmail.com>
2026-09-10 15:13:00 -0300

    feature: primeira-versao
    primeira versão do código da média escolar + readme atualizado

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Calculando-Media-Escolar
 Um código simples onde sua principal função é calcular média escolar.
+<br>
+<br>
+# Como funciona?
+• Primeiro você vai dizer o seu nome;
+
+<br>
+
+• Segundo você vai dizer as 4 notas do seu Bimestre;
+
+<br>
+
+• Terceiro o calculo vai ser efetuado e mostrará a sua média, e seu desempenho escolar.
---
+++ calculando-media-escolar.py
@@ -0,0 +1,13 @@
+print('CALCULANDO MÉDIA ESCOLAR')
+print('==========================')
+
+nome_aluno = input('Diga seu nome: ')
+
+nota1 = float(input('Digite sua primeira nota: '))
+nota2 = float(input('Digite sua segunda nota: '))
+nota3 = float(input('Digite sua terceira nota: '))
+nota4 = float(input('Digite sua quarta nota: '))
+
+media = ((nota1 + nota2 + nota3 + nota4)/4)
+
+print(f'{nome_aluno.title()}, sua média foi {media}')
